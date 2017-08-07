95e921362b3d702d7a4d45652fcfeba6323d6dc3
as3mbus <asembus@gmail.com>
2017-08-07 17:41:41 +0700

    Interaction and Dialogue added

---
+++ Assets/Scripts/DialogueControl.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class DialogueControl : MonoBehaviour
+{
+
+
+    public GameObject dPanel, center;
+    public Text dText;
+
+    public bool dialogueActive;
+
+    // Use this for initialization
+    void Start()
+    {
+        dPanel.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            dPanel.SetActive(!dialogueActive);
+            dialogueActive = !dialogueActive;
+            FindObjectOfType<PlayerControl>().interact=false;
+        }
+    }
+    public void showDialogue(string dialogue)
+    {
+		dialogueActive=true;
+        dPanel.SetActive(true);
+        dText.text = dialogue;
+    }
+
+}
---
+++ Assets/Scripts/DialogueHolder.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DialogueHolder : MonoBehaviour {
+
+	public string dialogue;
+	private DialogueControl dControl;
+	// Use this for initialization
+	void Start () {
+		dControl=FindObjectOfType<DialogueControl>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+	public void interaction(){
+		dControl.showDialogue(dialogue);
+	}
+}
