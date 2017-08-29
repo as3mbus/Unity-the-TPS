f22a19117997a0b219750c9a8102d08665f133ef
Lab mulmed <labmulmed123@gmail.com>
2017-08-29 15:13:02 +0700

    attack trigger and animation[STILL IN PROGRESS]

---
+++ Assets/Scripts/AttackTrigger.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AttackTrigger : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	void OnTriggerEnter(Collider other){
+		other.transform.Translate(transform.parent.parent.parent.forward * 2);
+		print("arms triggered");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
--- Assets/Scripts/PlayerControl.cs
+++ Assets/Scripts/PlayerControl.cs
@@ -6,7 +6,7 @@ public class PlayerControl : MonoBehaviour
 {
 
     public Transform center;
-    public Transform body;
+    public GameObject body;
     public bool interact = false;
 
     public float jumpForce;
@@ -24,40 +24,42 @@ public class PlayerControl : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (!interact)
-        {
+        if (interact) return;
 
-            if (IsGrounded())
-            {
-                jumpDir.y = 0;
-            }
-            //jump
-            if (Input.GetButtonDown("Jump") && IsGrounded())
-            {
-                //GetComponent<Rigidbody>().AddForce(0,jumpForce,0);
-                jumpDir.y = jumpForce;
-            }
-
-            //gravity
-            if (!IsGrounded())
-            {
-                jumpDir.y -= gravity * Time.deltaTime;
-
-            }
-
-            //move for jump and gravity.
-            GetComponent<CharacterController>().Move(jumpDir * Time.deltaTime);
-
-            //read input
-            float moveHorizontal = Input.GetAxis("Horizontal");
-            float moveVertical = Input.GetAxis("Vertical");
-            Vector3 movement = transform.forward * moveVertical + transform.right * moveHorizontal;
-            //Move
-            GetComponent<CharacterController>().Move(movement * Time.deltaTime* speed);
-            var tolook = center.transform.position + transform.forward * 2;
-            tolook.y=0;
-            //body.LookAt(tolook);
+        if(Input.GetButtonDown("Fire1")){
+            body.GetComponentInChildren<Animation>().Play();
         }
+        if (GetComponent<CharacterController>().isGrounded)
+        {
+            jumpDir.y = 0;
+        }
+        //jump
+        if (Input.GetButtonDown("Jump") && IsGrounded())
+        {
+            //GetComponent<Rigidbody>().AddForce(0,jumpForce,0);
+            jumpDir.y = jumpForce;
+        }
+
+        //gravity
+        if (!GetComponent<CharacterController>().isGrounded)
+        {
+            jumpDir.y -= gravity * Time.deltaTime;
+
+        }
+
+        //move for jump and gravity.
+        GetComponent<CharacterController>().Move(jumpDir * Time.deltaTime);
+
+        //read input
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+        Vector3 movement = transform.forward * moveVertical + transform.right * moveHorizontal;
+        //Move
+        GetComponent<CharacterController>().Move(movement * Time.deltaTime * speed);
+        var tolook = center.transform.position + transform.forward * 2;
+        tolook.y = 0;
+        //body.LookAt(tolook);
+
     }
 
 
